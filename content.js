ceab8c525474fbb5eb2dca1888e2c4e6d20ce8e6
Gavri Kepets <gkepets@gmail.com>
2016-12-14 18:36:22 -0500

    Added CSS and Starting on the Tine/Date
    It works! Kinda.

--- content.js
+++ content.js
@@ -1,5 +1,18 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-  $('head').append('hello');
+  var currentdate = new Date(); 
+(function(d) {
+	var date = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear();
+	
+  d.getElementsByTagName("date")[0].innerHTML = date;
+})(this.document);
+(function(c){
+	var time = currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();			
+	  c.getElementsByTagName("time")[0].innerHTML = time;
 
-);
+})(this.document);
+setTimeout(function(){
+   window.location.reload(1);
+}, 1000);
