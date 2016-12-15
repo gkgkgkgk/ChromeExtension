71a82b1eda6c040c92dade6963f7166f1166c92b
Gavri Kepets <gkepets@gmail.com>
2016-12-14 19:11:19 -0500

    Name, Date, Time

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
-  var currentdate = new Date(); 
+var currentdate = new Date(); 
+
+(function(name) {
+	var nameString = "Gavri";
+	name.getElementsByTagName("name")[0].innerHTML = "Welcome,"+ nameString;
+})(this.document);
+
 (function(d) {
 	var date = currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
@@ -6,6 +12,8 @@
 	
   d.getElementsByTagName("date")[0].innerHTML = date;
 })(this.document);
+
+
 (function(c){
 	var time = currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
@@ -13,6 +21,7 @@
 	  c.getElementsByTagName("time")[0].innerHTML = time;
 
 })(this.document);
+
 setTimeout(function(){
    window.location.reload(1);
 }, 1000);
