fdd881d62d0daac5c1bf6e674634450c3295f17d
Gavri Kepets <gkepets@gmail.com>
2016-12-15 13:10:35 -0500

    Huge Changes!
    Got the SentInterval function, and also got the time zones working!

--- content.js
+++ content.js
@@ -1,8 +1,26 @@
+//*****************************//
+// running once to initialize //
 var currentdate = new Date(); 
+var welcomeMessage = "";
+if(currentdate.getHours() < 11 && currentdate.getHours() > 4){
+	welcomeMessage = "Good Morning, "
+	}
+else if(currentdate.getHours() >= 12){
+	welcomeMessage = "Good Afternoon, "
+	}
+else if(currentdate.getHours() >= 17){
+	welcomeMessage = "Good Evening, "
+	}
+else if(currentdate.getHours() >= 21 && currentdate.getHours() <= 4){
+	welcomeMessage = "Good Afternoon "
+	}
+else {
+	welcomeMessage = "Ya failed!";
+	}
 
 (function(name) {
 	var nameString = "Gavri";
-	name.getElementsByTagName("name")[0].innerHTML = "Welcome,"+ nameString;
+	name.getElementsByTagName("name")[0].innerHTML = welcomeMessage + nameString;
 })(this.document);
 
 (function(d) {
@@ -20,8 +38,49 @@ var currentdate = new Date();
                 + currentdate.getSeconds();			
 	  c.getElementsByTagName("time")[0].innerHTML = time;
 
+})(this.document);  
+//*****************************//
+
+//____________________//
+setInterval(function(){
+var welcomeMessage = "";
+var currentdate = new Date(); 
+if(currentdate.getHours() < 11 && currentdate.getHours() > 4){
+	welcomeMessage = "Good Morning, "
+	}
+else if(currentdate.getHours() >= 12){
+	welcomeMessage = "Good Afternoon, "
+	}
+else if(currentdate.getHours() >= 17){
+	welcomeMessage = "Good Evening, "
+	}
+else if(currentdate.getHours() >= 21 && currentdate.getHours() <= 4){
+	welcomeMessage = "Good Afternoon "
+	}
+else {
+	welcomeMessage = "Ya failed!";
+	}
+
+
+(function(name) {
+	var nameString = "Gavri";
+	name.getElementsByTagName("name")[0].innerHTML = welcomeMessage + nameString;
 })(this.document);
 
-setTimeout(function(){
-   window.location.reload(1);
+(function(d) {
+	var date = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear();
+	
+  d.getElementsByTagName("date")[0].innerHTML = date;
+})(this.document);
+
+
+(function(c){
+	var time = currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();			
+	  c.getElementsByTagName("time")[0].innerHTML = time;
+
+})(this.document);   
 }, 1000);
