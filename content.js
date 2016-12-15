a36d754c60eae021f471b75a87e7811c5d3949e8
Gavri Kepets <gkepets@gmail.com>
2016-12-15 18:30:29 -0500

    New Layout!
    Added Bootstrap!

--- content.js
+++ content.js
@@ -1,86 +1,83 @@
 //*****************************//
-// running once to initialize //
 var currentdate = new Date(); 
 var welcomeMessage = "";
-if(currentdate.getHours() < 11 && currentdate.getHours() > 4){
+var dateDay = currentdate.getDate();
+var month = currentdate.getMonth()
+var year = currentdate.getFullYear();
+var hours = currentdate.getHours();
+var minutes = currentdate.getMinutes();
+var seconds = currentdate.getSeconds();
+var status = "AM";
+setWelcomeMessage();
+setName();
+setTheDate();
+setTheTime();
+function setWelcomeMessage(){
+if(hours < 11 && hours > 4){
 	welcomeMessage = "Good Morning, "
 	}
-else if(currentdate.getHours() >= 12){
+else if(hours  >= 12 && hours < 17){
 	welcomeMessage = "Good Afternoon, "
 	}
-else if(currentdate.getHours() >= 17){
+else if(hours >= 17 && hours < 21){
 	welcomeMessage = "Good Evening, "
 	}
-else if(currentdate.getHours() >= 21 && currentdate.getHours() <= 4){
+else if(hours >= 21 && hours <= 4){
 	welcomeMessage = "Good Afternoon "
 	}
 else {
-	welcomeMessage = "Ya failed!";
+	welcomeMessage = "Out of bounds";
 	}
-
+}
+function setName(){
 (function(name) {
 	var nameString = "Gavri";
 	name.getElementsByTagName("name")[0].innerHTML = welcomeMessage + nameString;
 })(this.document);
+}
 
+function setTheDate(){
 (function(d) {
-	var date = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear();
+	var date = (month+1) + "/"
+                + dateDay  + "/" 
+                + year;
 	
   d.getElementsByTagName("date")[0].innerHTML = date;
 })(this.document);
+}
 
-
+function setTheTime(){
 (function(c){
-	var time = currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();			
+	var newHours = hours;
+	if(hours > 12){
+		status = "PM";
+		newHours -= 12;
+		}
+	else{
+		newHours = hours;
+		status = "AM";
+		}
+	var time = newHours + ":"  
+                + minutes + ":" 
+                + seconds + " "+
+                status;		
 	  c.getElementsByTagName("time")[0].innerHTML = time;
 
-})(this.document);  
+})(this.document); 
+}
 //*****************************//
-
-//____________________//
 setInterval(function(){
-var welcomeMessage = "";
-var currentdate = new Date(); 
-if(currentdate.getHours() < 11 && currentdate.getHours() > 4){
-	welcomeMessage = "Good Morning, "
-	}
-else if(currentdate.getHours() >= 12){
-	welcomeMessage = "Good Afternoon, "
-	}
-else if(currentdate.getHours() >= 17){
-	welcomeMessage = "Good Evening, "
-	}
-else if(currentdate.getHours() >= 21 && currentdate.getHours() <= 4){
-	welcomeMessage = "Good Afternoon "
-	}
-else {
-	welcomeMessage = "Ya failed!";
-	}
-
-
-(function(name) {
-	var nameString = "Gavri";
-	name.getElementsByTagName("name")[0].innerHTML = welcomeMessage + nameString;
-})(this.document);
-
-(function(d) {
-	var date = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear();
-	
-  d.getElementsByTagName("date")[0].innerHTML = date;
-})(this.document);
-
-
-(function(c){
-	var time = currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();			
-	  c.getElementsByTagName("time")[0].innerHTML = time;
-
-})(this.document);   
+//get the variables
+hours = currentdate.getHours();
+year = currentdate.getFullYear();
+hours = currentdate.getHours();
+minutes = currentdate.getMinutes();
+seconds = currentdate.getSeconds();
+currentdate = new Date(); 
+welcomeMessage = "";
+//function
+setWelcomeMessage();
+setName();
+setTheDate();
+setTheTime();   
 }, 1000);
