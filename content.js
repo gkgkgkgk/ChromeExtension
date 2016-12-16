0c921a72b9e8982e1c666d6984ecdfb77651132e
Gavri Kepets <gkepets@gmail.com>
2016-12-16 15:02:18 -0500

    Weather API implemented

--- content.js
+++ content.js
@@ -9,6 +9,7 @@ var hours = currentdate.getHours();
 var minutes = currentdate.getMinutes();
 var seconds = currentdate.getSeconds();
 var status = "AM";
+var weatherString = "weather";
 setWelcomeMessage();
 setName();
 setTheDate();
@@ -64,23 +65,50 @@ function setTheTime(){
 
 })(this.document); 
 }
+
+//***********WEATHER API AND FUNCTION***********//
+
+$.ajax({
+	url:"https://api.darksky.net/forecast/62296b6b9da67149d9f8d8f4e2949f**/40.893247,-74.011654",
+	method:"GET"
+	}).done(function(response){
+		console.log(response.currently.temperature);
+if(response.currently.temperature< 0){
+	weatherString = "Its "+response.currently.temperature+" degrees..... ITS FREEZING!";
+}
+else if(response.currently.temperature< 32){
+	weatherString = "Its "+response.currently.temperature+" degrees..... bundle up!";
+}
+else if(response.currently.temperature< 50){
+	weatherString = "Its "+response.currently.temperature+" degrees outside..... you need a light jacket.";
+}
+else if(response.currently.temperature< 80){
+	weatherString = "Its "+response.currently.temperature+" degrees outside..... no need for extra layers!";
+}
+else if(response.currently.temperature< 100){
+	weatherString = "Its "+response.currently.temperature+" degrees outside..... wear shorts and a t-shirt!";
+}
+else {
+	weatherString = "Its "+response.currently.temperature+" degrees..... its boiling out.";
+}
+setWeather();
+});
+
+
 function setWeather(){
 	(function(weather){
-		  weather.getElementsByTagName("weather")[0].innerHTML = "you win";
+		  weather.getElementsByTagName("weather")[0].innerHTML = weatherString;
 })(this.document); }
+//***********END OF WEATHER API AND FUNCTION***********//
+
+
+
+
+
 
 //*****************************//
 setInterval(function(){
 //get the variables
-$.ajax({
-	url:"https://api.darksky.net/forecast/PUT_KEY_HERE/40.893247,-74.011654",
-	method:"GET"
-	}).done(function(response){
-console.log(response.currently.temperature);
-
-
-});
-
 hours = currentdate.getHours();
 year = currentdate.getFullYear();
 hours = currentdate.getHours();
